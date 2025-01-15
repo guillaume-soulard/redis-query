499c7f23b04a7bfcf44f0ca57c921fcb8235060e
guillaume.soulard <guillaume.soulard.fr@gmail.com>
2025-01-15 11:19:44 +0100

    WIP

--- subcommand_connect.go
+++ subcommand_connect.go
@@ -9,7 +9,67 @@ import (
 	"strings"
 )
 
-var commandMap []interface{}
+type FindKeySpec struct {
+	LastKey int
+	KeyStep int
+	Limit   int
+}
+
+type FindKeySearchType string
+
+const (
+	FindKeySearchTypeRange = "range"
+	FindKeySearchTypeKeynum = "keynum"
+	FindKeySearchTypeUnknown = "unknown"
+)
+
+https://redis.io/docs/latest/develop/reference/key-specs/
+type FindKey struct {
+	Type FindKeySearchType
+	RangeSpec FindKeyRangeSpec
+	KeyNumSpec FindKeyKeyNumSpec
+}
+
+type BeginSearchIndexSpec struct {
+	Index int
+}
+type BeginSearchKeywordSpec struct {
+	KeyWord string
+	StartFrom int
+}
+
+type BeginSearchType string
+
+const (
+	BeginSearchTypeIndex = "index"
+	BeginSearchTypeKeyword = "keyword"
+	BeginSearchTypeUnknown = "unknown"
+)
+
+type BeginSearchSpec struct {
+	Type      BeginSearchType
+	IndexSpec BeginSearchIndexSpec
+	KeyWordSpec BeginSearchKeywordSpec
+}
+
+type KeySpecification struct {
+	BeginSearch BeginSearchSpec
+	FindKey     FindKey
+	Notes       string
+}
+
+type CommandInfo struct {
+	Name              string
+	FirstKey          int
+	LastKey           int
+	StepKey           int
+	Tips              string
+	KeySpecifications []KeySpecification
+	SubCommands       map[string]CommandInfo
+}
+
+var commands []interface{}
+var commandMap map[string]CommandInfo
 var lastResult = make([]string, 0)
 
 type ConnectSubCommand struct{}
@@ -32,12 +92,17 @@ func loadCompletion(client *redis.Client) (err error) {
 	if result, err = client.Do(context.Background(), "COMMAND").Result(); err != nil {
 		return err
 	}
-	commandMap = result.([]interface{})
+	commands = result.([]interface{})
 	return err
 }
 
 func completer(d prompt.Document) []prompt.Suggest {
 	items := strings.Split(d.Text, " ")
+	var commandName string
+	if len(items) > 0 {
+		commandName = items[0]
+	}
+	isCurrentArgKey, commandExists := commandMap[commandName]
 	s := make([]prompt.Suggest, 0)
 	if len(items) > 1 && len(lastResult) > 0 {
 		for _, r := range lastResult {
@@ -46,7 +111,7 @@ func completer(d prompt.Document) []prompt.Suggest {
 			})
 		}
 	} else if len(items) == 1 {
-		for _, command := range commandMap {
+		for _, command := range commands {
 			info := command.([]interface{})
 			commandName := fmt.Sprintf("%v", info[0])
 			s = append(s, prompt.Suggest{
