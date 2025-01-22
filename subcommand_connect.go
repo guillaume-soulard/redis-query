3e0d480cdf82698862fc16336f6d6a396660c318
guillaume.soulard <guillaume.soulard.fr@gmail.com>
2025-01-22 09:29:00 +0100

    Fix tty on exit

--- subcommand_connect.go
+++ subcommand_connect.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/c-bata/go-prompt"
 	"github.com/go-redis/redis/v8"
+	"os"
+	"os/exec"
 	"slices"
 	"strings"
 )
@@ -54,29 +56,47 @@ func completer(d prompt.Document) []prompt.Suggest {
 	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
 
-func showPrompt(client *redis.Client) (err error) {
-	history := make([]string, 0, 10)
-	for {
-		command := prompt.Input("redis> ", completer,
-			prompt.OptionHistory(history),
-			prompt.OptionCompletionOnDown(),
-			prompt.OptionShowCompletionAtStart(),
-		)
+var history []string
+var globalClient *redis.Client
+
+func executor(command string) {
+	if len(history) == 0 || history[len(history)-1] != command {
 		history = append(history, command)
-		var result interface{}
-		if strings.ToLower(command) == "exit" {
-			break
-		}
-		argsStr := strings.Split(command, " ")
-		args := make([]interface{}, len(argsStr))
-		for i, a := range argsStr {
-			args[i] = a
-		}
-		if result, err = client.Do(context.Background(), args...).Result(); err != nil {
-			printResult(err)
-		} else {
-			lastResult = printResult(result)
-		}
 	}
+	var result interface{}
+	if strings.ToLower(command) == "exit" {
+		rawModeOff := exec.Command("/bin/stty", "-raw", "echo")
+		rawModeOff.Stdin = os.Stdin
+		_ = rawModeOff.Run()
+		if err := rawModeOff.Wait(); err != nil {
+			PrintErrorAndExit(err)
+		}
+		os.Exit(0)
+	}
+	argsStr := strings.Split(command, " ")
+	args := make([]interface{}, len(argsStr))
+	for i, a := range argsStr {
+		args[i] = a
+	}
+	var err error
+	if result, err = globalClient.Do(context.Background(), args...).Result(); err != nil {
+		printResult(err)
+	} else {
+		lastResult = printResult(result)
+	}
+}
+
+func showPrompt(client *redis.Client) (err error) {
+	history = make([]string, 0, 10)
+	globalClient = client
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionHistory(history),
+		prompt.OptionCompletionOnDown(),
+		prompt.OptionShowCompletionAtStart(),
+		prompt.OptionPrefix("redis>"),
+	)
+	p.Run()
 	return err
 }
